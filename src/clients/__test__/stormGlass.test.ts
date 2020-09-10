a6f904af47d18ff286961313fdd7c94b2502d454
stephbsb <stephany0303@gmail.com>
2020-09-10 14:28:35 -0300

    adding tests for incomplete data and exception

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,59 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalizedResponse3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  it('should redturn the normalized forecas from the StormGlass service', async () => {
+    /* Praia da Reserva - RJ */
+    const lat = -23.0389424;
+    const lng = -43.4762407;
+
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalizedResponse3HoursFixture);
+  });
+
+  it('should exclude imcomplete data points', async () => {
+    const lat = -23.0389424;
+    const lng = -43.4762407;
+
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2020-04-26T00:00:00+00:00',
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual([]);
+  });
+
+  it('should get a generic error from StormGlass service when the request fails before reaching the service', async () => {
+    const lat = -23.0389424;
+    const lng = -43.4762407;
+
+    mockedAxios.get.mockRejectedValue({ message: 'Network Error' });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: NetworkError'
+    );
+  });
+});
